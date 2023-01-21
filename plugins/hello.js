979d6e85aa2b42801e3af25c2ee4c6586b7a04de
YouRanCoder <205545151@qq.com>
2023-01-21 17:36:50 +0800

    修复了测试无法执行的Bug,同步token

--- plugins/hello.js
+++ plugins/hello.js
@@ -10,9 +10,7 @@ bot.on("message", function (msg) {
 		this.setNickname(this.name)
 		msg.reply("成功", true) //改为false则不会引用
 		}}
-})
-bot.on("message",function(msg){
-	if (msg.raw_message === '测试' || msg.raw_message==='在吗'){
-		msg.reply('测试成功,我在的捏',true)
-	}
+    else if (msg.raw_message === '测试' || msg.raw_message==='在吗'){
+      msg.reply('测试成功,我在的捏',true)
+    }
 })
